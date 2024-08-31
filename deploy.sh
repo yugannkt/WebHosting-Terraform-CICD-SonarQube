00499ba9c8e1eab375751be03a4336b126534704
yugannkt <166296076+yugannkt@users.noreply.github.com>
2024-08-31 20:53:34 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo systemctl start apache2
+sudo rm -rf /var/www/html/*
+sudo cp -r * /var/www/html/
+sudo chown -R www-data:www-data /var/www/html/*
+sudo chmod -R 755 /var/www/html/*
